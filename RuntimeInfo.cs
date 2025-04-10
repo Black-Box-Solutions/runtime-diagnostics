cd50045f09804bfc903bade2287fe0858db9af45
Dave Black <656118+udlose@users.noreply.github.com>
2025-04-10 17:52:19 -0500

    Update RuntimeInfo class with new properties and methods
    Enhanced the `RuntimeInfo` class in the `BlackBoxSolutions.Diagnostics` namespace by adding several new properties: `GCMaxGeneration`, `Is64BitOperatingSystem`, `Is64BitProcess`, `IsNetCore`, `IsServerGC`, and `ProcessorCount`. Existing properties have been reordered for better organization. The `ToString` method has been updated to include the new properties, providing a more comprehensive overview of the runtime information.

--- RuntimeInfo.cs
+++ RuntimeInfo.cs
@@ -11,18 +11,24 @@ namespace BlackBoxSolutions.Diagnostics
     /// </summary>
     public class RuntimeInfo
     {
-        public string RuntimeVersion { get; private set; }
-        public string RuntimeDescription { get; private set; }
-#if NET5_0_OR_GREATER
-    public string RuntimeIdentifier { get; private set; }
-#endif
-        public string OSDescription { get; private set; }
         public string Architecture { get; private set; }
-        public bool IsJITOptimized { get; private set; }
-        public string GCMode { get; private set; }
-        public bool TieredCompilationEnabled { get; private set; }
         public string CoreLibInfoVersion { get; private set; }
         public string CoreLibLocation { get; private set; }
+        public int GCMaxGeneration { get; private set; }
+        public bool Is64BitOperatingSystem { get; private set; }
+        public bool Is64BitProcess { get; private set; }
+        public bool IsJITOptimized { get; private set; }
+        public bool IsNetCore { get; private set; }
+        public bool IsServerGC { get; private set; }
+        public string OSDescription { get; private set; }
+        public int ProcessorCount { get; private set; }
+#if NET5_0_OR_GREATER
+        public string RuntimeIdentifier { get; private set; }
+#endif
+        public string RuntimeDescription { get; private set; }
+        public string RuntimeDirectory { get; private set; }
+        public string RuntimeVersion { get; private set; }
+        public bool TieredCompilationEnabled { get; private set; }
 
         public static RuntimeInfo GetRuntimeInfo()
         {
@@ -31,27 +37,33 @@ namespace BlackBoxSolutions.Diagnostics
 
             return new RuntimeInfo
             {
-                RuntimeVersion = Environment.Version.ToString(),
-                RuntimeDescription = RuntimeInformation.FrameworkDescription,
-#if NET5_0_OR_GREATER
-            RuntimeIdentifier = RuntimeInformation.RuntimeIdentifier,
-#endif
-                OSDescription = RuntimeInformation.OSDescription,
                 Architecture = RuntimeInformation.ProcessArchitecture.ToString(),
-                IsJITOptimized = DiagnosticMechanics.IsOptimizedBuild(executingAssembly),
-                GCMode = GCSettings.IsServerGC ? "Server" : "Workstation",
-                TieredCompilationEnabled = DiagnosticMechanics.IsTieredCompilationEnabled(),
                 CoreLibInfoVersion = coreLibAssembly.GetName().Version?.ToString() ?? "Unknown",
-                CoreLibLocation = coreLibAssembly.Location
+                CoreLibLocation = coreLibAssembly.Location,
+                GCMaxGeneration = GC.MaxGeneration,
+                Is64BitOperatingSystem = Environment.Is64BitOperatingSystem,
+                Is64BitProcess = Environment.Is64BitProcess,
+                IsJITOptimized = DiagnosticMechanics.IsOptimizedBuild(executingAssembly),
+                IsNetCore = Type.GetType("System.Runtime.InteropServices.RuntimeInformation, System.Runtime.InteropServices.RuntimeInformation") != null,
+                IsServerGC = GCSettings.IsServerGC,
+                OSDescription = RuntimeInformation.OSDescription,
+                ProcessorCount = Environment.ProcessorCount,
+#if NET5_0_OR_GREATER
+                RuntimeIdentifier = RuntimeInformation.RuntimeIdentifier,
+#endif
+                RuntimeDescription = RuntimeInformation.FrameworkDescription,
+                RuntimeDirectory = AppContext.BaseDirectory,
+                RuntimeVersion = Environment.Version.ToString(),
+                TieredCompilationEnabled = DiagnosticMechanics.IsTieredCompilationEnabled()
             };
         }
 
         public override string ToString()
         {
 #if NET5_0_OR_GREATER
-        return $"{nameof(RuntimeVersion)}: {RuntimeVersion}, {nameof(RuntimeDescription)}: {RuntimeDescription}, {nameof(RuntimeIdentifier)}: {RuntimeIdentifier}, {nameof(OSDescription)}: {OSDescription}, {nameof(Architecture)}: {Architecture}, {nameof(IsJITOptimized)}: {IsJITOptimized}, {nameof(TieredCompilationEnabled)}: {TieredCompilationEnabled}, {nameof(GCMode)}: {GCMode}, {nameof(CoreLibInfoVersion)}: {CoreLibInfoVersion}, {nameof(CoreLibLocation)}: {CoreLibLocation}";
+            return $"{nameof(Architecture)}: {Architecture}, {nameof(CoreLibInfoVersion)}: {CoreLibInfoVersion}, {nameof(CoreLibLocation)}: {CoreLibLocation}, {nameof(GCMaxGeneration)}: {GCMaxGeneration}, {nameof(Is64BitOperatingSystem)}: {Is64BitOperatingSystem}, {nameof(Is64BitProcess)}: {Is64BitProcess}, {nameof(IsJITOptimized)}: {IsJITOptimized}, {nameof(IsNetCore)}: {IsNetCore}, {nameof(IsServerGC)}: {IsServerGC}, {nameof(OSDescription)}: {OSDescription}, {nameof(ProcessorCount)}: {ProcessorCount}, {nameof(RuntimeDescription)}: {RuntimeDescription}, {nameof(RuntimeDirectory)}: {RuntimeDirectory}, {nameof(RuntimeIdentifier)}: {RuntimeIdentifier}, {nameof(RuntimeVersion)}: {RuntimeVersion}, {nameof(TieredCompilationEnabled)}: {TieredCompilationEnabled}";
 #else
-            return $"{nameof(RuntimeVersion)}: {RuntimeVersion}, {nameof(RuntimeDescription)}: {RuntimeDescription}, {nameof(OSDescription)}: {OSDescription}, {nameof(Architecture)}: {Architecture}, {nameof(IsJITOptimized)}: {IsJITOptimized}, {nameof(TieredCompilationEnabled)}: {TieredCompilationEnabled}, {nameof(GCMode)}: {GCMode}, {nameof(CoreLibInfoVersion)}: {CoreLibInfoVersion}, {nameof(CoreLibLocation)}: {CoreLibLocation}";
+            return $"{nameof(Architecture)}: {Architecture}, {nameof(CoreLibInfoVersion)}: {CoreLibInfoVersion}, {nameof(CoreLibLocation)}: {CoreLibLocation}, {nameof(GCMaxGeneration)}: {GCMaxGeneration}, {nameof(Is64BitOperatingSystem)}: {Is64BitOperatingSystem}, {nameof(Is64BitProcess)}: {Is64BitProcess}, {nameof(IsJITOptimized)}: {IsJITOptimized}, {nameof(IsNetCore)}: {IsNetCore}, {nameof(IsServerGC)}: {IsServerGC}, {nameof(OSDescription)}: {OSDescription}, {nameof(ProcessorCount)}: {ProcessorCount}, {nameof(RuntimeDescription)}: {RuntimeDescription}, {nameof(RuntimeDirectory)}: {RuntimeDirectory}, {nameof(RuntimeVersion)}: {RuntimeVersion}, {nameof(TieredCompilationEnabled)}: {TieredCompilationEnabled}";
 #endif
         }
     }
